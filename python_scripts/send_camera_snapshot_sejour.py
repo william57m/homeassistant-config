e6dc90b95fd976d3e65626ade13b45c4f7c9ceae
William Mura <william57m@gmail.com>
2020-05-09 19:32:44 -0400

    Update scripts

--- python_scripts/send_camera_snapshot_sejour.py
+++ python_scripts/send_camera_snapshot_sejour.py
@@ -16,11 +16,9 @@ foscamIp = '192.168.2.118'
 foscamPort = '88'
 foscamSnapshotUrl = f'http://{foscamIp}:{foscamPort}/cgi-bin/CGIProxy.fcgi\\?cmd\\=snapPicture2\\&usr\\={foscamUsername}\\&pwd\\={foscamPassword}'
 
-# Notification
-message = 'Porte entrée ouverte'
-
-# Send notification
-os.system(f'curl --data chat_id={chatId} --data-urlencode "text={message}" "https://api.telegram.org/bot{apiKey}/sendMessage" ')
+# Get snapshot
 os.system(f'curl {foscamSnapshotUrl} -o /home/homeassistant/.homeassistant/tmp/snapshot.jpg')
+
+# Send photo
 os.system(f'curl -s -X POST "https://api.telegram.org/bot{apiKey}/sendPhoto" -F chat_id={chatId} -F photo="@/home/homeassistant/.homeassistant/tmp/snapshot.jpg" ')
 
