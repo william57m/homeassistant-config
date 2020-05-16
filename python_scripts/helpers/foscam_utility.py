99bc9e5b1bbfd670000c40fecf5a58740eafd609
William Mura <william57m@gmail.com>
2020-05-15 21:27:29 -0400

    Add script to get motion sensor

--- python_scripts/helpers/foscam_utility.py
+++ python_scripts/helpers/foscam_utility.py
@@ -1,4 +1,5 @@
 import requests
+import xmltodict
 
 
 def set_motion_sensor(ip, port, user, password, state=1):
@@ -7,6 +8,17 @@ def set_motion_sensor(ip, port, user, password, state=1):
     :param state: 0 to disable, 1 to enable
   '''
   requests.get(f'http://{ip}:{port}/cgi-bin/CGIProxy.fcgi?cmd=setMotionDetectConfig&isEnable={state}&usr={user}&pwd={password}')
+
+
+def get_motion_sensor_state(ip, port, user, password):
+  '''
+  Get motion sensor state
+    :return: 0 when disabled, 1 when enabled
+  '''
+  response = requests.get(f'http://{ip}:{port}/cgi-bin/CGIProxy.fcgi?cmd=getMotionDetectConfig&usr={user}&pwd={password}')
+  doc = xmltodict.parse(response.content)
+  result = doc['CGI_Result']['isEnable'][0]
+  print(result)
 
 
 def download_snapshot(ip, port, user, password, to_path):
