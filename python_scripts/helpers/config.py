95926dfdb272052ca1d3d1e84b701b57c3bb387a
William Mura <william.mura@autodesk.com>
2020-10-29 13:29:35 -0400

    Update config python

--- python_scripts/helpers/config.py
+++ python_scripts/helpers/config.py
@@ -1,5 +1,4 @@
 # Paths
-BASE_PATH = '/Users/muraw/Development/config-domo/src/homeassistant'
-# BASE_PATH = '/home/homeassistant/.homeassistant'
+BASE_PATH = '/home/homeassistant/.homeassistant'
 SECRETS_PATH = BASE_PATH + '/' + 'secrets.yaml'
 SNAPSHOTS_PATH = BASE_PATH + '/' + 'tmp'
